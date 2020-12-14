f050bc294c0015a28e2612b59cc3525f297e686d
ItsmeHJB <harrison.be@hotmail.co.uk>
2020-12-14 18:25:44 +0000

    Updated codebase to retrieve referral data too

--- esd-group-3/src/java/models/Referrals.java
+++ esd-group-3/src/java/models/Referrals.java
@@ -6,8 +6,12 @@
 package models;
 
 import dbcon.DBConnection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -17,8 +21,8 @@ import java.util.logging.Logger;
  */
 public class Referrals {
     private int clientid;
-    private String name;
-    private String address;
+    private String[] name;
+    private String[] address;
     
     public void setClientId(int clientid) {
         this.clientid = clientid;
@@ -28,20 +32,33 @@ public class Referrals {
         return this.clientid;
     }
     
-    public void setName(String name) {
+    public void setName(String[] name) {
         this.name = name;
     }
     
-    public String getName() {
+    public String[] getName() {
         return this.name;
     }
     
-    public void setAddress(String address) {
+    public void setAddress(String[] address) {
         this.address = address;
     }
     
-    public String getAddress() {
+    public String[] getAddress() {
         return this.address;
+    }
+    
+    private int CountResultSetSize(ResultSet rs) {
+        int size = 1;
+        try {
+            if (rs.next() == true) {
+                size++;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Referrals.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return size;
     }
     
     public void create(
@@ -50,9 +67,9 @@ public class Referrals {
         String name,
         String address
     ) {
-        String cid = String.valueOf(clientid); //Convert clientid to string for query
+        // String cid = String.valueOf(clientid); //Convert clientid to string for query
         String query = "INSERT INTO Referrals (clientid, name, address) VALUES"
-                + "('" + cid + "', '" + name + "', '" + address + "')";
+                + "(" + clientid + ", '" + name + "', '" + address + "')";
         
         try (Statement stmt = dbcon.conn.createStatement()) {
             stmt.execute(query);
@@ -60,8 +77,44 @@ public class Referrals {
             Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
         }
         
+        String[] nameArr = {name};
+        String[] addressArr = {address};
+        
         this.setClientId(clientid);
-        this.setName(name);
-        this.setAddress(address);
+        this.setName(nameArr);
+        this.setAddress(addressArr);
+    }
+    
+    public void retreiveAll (
+        DBConnection dbcon,
+        int clientid
+    ) {
+        String cid = String.valueOf(clientid);
+        String query = "SELECT name, address FROM Referrals WHERE clientid = "
+                + cid;
+        
+        List<String> nameList = new ArrayList<String>();
+        List<String> addressList = new ArrayList<String>();
+        
+        try (Statement stmt = dbcon.conn.createStatement()) {
+            ResultSet resultSet = stmt.executeQuery(query);
+            while (resultSet.next()) {
+                nameList.add(resultSet.getString("name"));
+                addressList.add(resultSet.getString("address"));
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        
+        String nameArr[] = new String[nameList.size()];
+        nameArr = nameList.toArray(nameArr);
+        String addressArr[] = new String[addressList.size()];
+        addressArr = addressList.toArray(addressArr);
+        
+        System.out.println(Arrays.toString(nameArr));
+        System.out.println(Arrays.toString(addressArr));
+        
+        this.setName(nameArr);
+        this.setAddress(addressArr);
     }
 }
