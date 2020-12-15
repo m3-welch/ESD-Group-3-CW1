30454ba6bc3985b73a6d38d59a16de1a41d5f89d
conranpearce <54678624+conranpearce@users.noreply.github.com>
2020-12-15 19:01:33 +0000

    added function to get id from Users table

--- esd-group-3/src/java/models/Bookings.java
+++ esd-group-3/src/java/models/Bookings.java
@@ -29,12 +29,8 @@ import java.time.LocalTime;
  */
 public class Bookings {
     
-    // Create a booking by appending data to the bookingslot table
-    public void createBooking(DBConnection dbcon, String username, String employeeUsername, Date date, Time startTime, Time endTime) {
-        String query = "";
-
-        // Get the user's id depending on the username
-        query = "SELECT id FROM Users WHERE username = '" + username + "'";
+    public int getID(DBConnection dbcon, String user) {
+        String query = "SELECT id FROM Users WHERE username = '" + user + "'";
         int userid = 0;
         try (Statement stmt = dbcon.conn.createStatement()) {
             ResultSet resultSet = stmt.executeQuery(query);
@@ -44,6 +40,16 @@ public class Bookings {
         } catch (SQLException e) {
             System.out.println(e);
         }
+        
+        return userid; 
+    }
+    
+    // Create a booking by appending data to the bookingslot table
+    public void createBooking(DBConnection dbcon, String username, String employeeUsername, Date date, Time startTime, Time endTime) {
+        String query = "";
+        
+        // Get the user's id depending on the username
+        int userid = getID(dbcon, username);
         
         // Get the client id from the userid in the Clients table
         query = "SELECT id FROM Clients WHERE userid = " + userid;
@@ -57,17 +63,7 @@ public class Bookings {
             System.out.println(e);
         }
         
-        // Get the id of the employee from the employee username
-        query = "SELECT id FROM Users WHERE username = '" + employeeUsername + "'";
-        int employeeidbefore = 0;
-        try (Statement stmt = dbcon.conn.createStatement()) {
-            ResultSet resultSet = stmt.executeQuery(query);
-            while (resultSet.next()) {
-                employeeidbefore = resultSet.getInt("id");
-            }
-        } catch (SQLException e) {
-            System.out.println(e);
-        }
+        int employeeidbefore = getID(dbcon, employeeUsername);
         
         // Get the employee id from the userid in the Employees table
         query = "SELECT id FROM Employees WHERE userid = " + employeeidbefore;
