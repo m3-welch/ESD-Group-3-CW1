4b281c7dcf9a219340eb1379fa2d5acc4dd29a8d
ItsmeHJB <harrison.be@hotmail.co.uk>
2020-12-15 16:36:36 +0000

    Prescriptions can be added and retrieved now

--- esd-group-3/src/java/models/Prescriptions.java
+++ esd-group-3/src/java/models/Prescriptions.java
@@ -6,9 +6,14 @@
 package models;
 
 import dbcon.DBConnection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -19,12 +24,12 @@ import java.util.logging.Logger;
 public class Prescriptions {
     private int clientid;
     private int prescription_count = 0;
-    private int[] employeeid;
-    private String[] drug_name;
-    private String[] dosage;
-    private Boolean[] is_repeat;
-    private LocalDate[] date_start;
-    private LocalDate[] date_end;
+    private int[] employeeid = new int[1];
+    private String[] drug_name = new String[1];
+    private String[] dosage = new String[1];
+    private Boolean[] is_repeat = new Boolean[1];
+    private LocalDate[] date_start = new LocalDate[1];
+    private LocalDate[] date_end = new LocalDate[1];
     
     private void setClientId(int clientid) {
         this.clientid = clientid;
@@ -98,7 +103,7 @@ public class Prescriptions {
         return this.date_end;
     }
     
-    public void addPrescription(
+    private void addPrescription(
         int employeeid,
         String drug_name,
         String dosage,
@@ -106,27 +111,41 @@ public class Prescriptions {
         LocalDate date_start,
         LocalDate date_end
     ) {
+        // Increase prescription amount
         this.increasePrescriptionCount();
         int new_pres_count = this.getPrescriptionCount();
         
-        //
-        String[] drugs = new String[new_pres_count];
-        drugs = this.getDrugName();
-        drugs[new_pres_count-1] = drug_name;
-        this.setDrugName(drugs);
+        // Get old data, insert into array of size new_pres_count
+        String[] drugs = Arrays.copyOf(this.getDrugName(), new_pres_count);
+        String[] doses = Arrays.copyOf(this.getDosage(), new_pres_count);
+        Boolean[] repeats = Arrays.copyOf(this.getIsRepeat(), new_pres_count);
+        LocalDate[] starts = Arrays.copyOf(this.getDateStart(), new_pres_count);
+        LocalDate[] ends = Arrays.copyOf(this.getDateEnd(), new_pres_count);
         
-        String[] old_dosage = this.getDosage();
+        // Attach new data
+        drugs[new_pres_count - 1] = drug_name;
+        doses[new_pres_count - 1] = dosage;
+        repeats[new_pres_count - 1] = is_repeat;
+        starts[new_pres_count - 1] = date_start;
+        ends[new_pres_count - 1] = date_end;
+        
+        // Assign to attributes using setters
+        this.setDrugName(drugs);
+        this.setDosage(doses);
+        this.setIsRepeat(repeats);
+        this.setDateStart(starts);
+        this.setDateEnd(ends);
     }
     
     public void create(
-        DBConnection dbcon,
-        int clientid,
-        int employeeid,
-        String drug_name,
-        String dosage,
-        Boolean is_repeat,
-        LocalDate date_start,
-        LocalDate date_end
+            DBConnection dbcon,
+            int clientid,
+            int employeeid,
+            String drug_name,
+            String dosage,
+            Boolean is_repeat,
+            LocalDate date_start,
+            LocalDate date_end
     ) {
         String query = "INSERT INTO Prescriptions (clientid, employeeid, "
                 + "drug_name, dosage, is_repeat, date_start, date_end) VALUES"
@@ -141,5 +160,64 @@ public class Prescriptions {
         }
         
         this.addPrescription(employeeid, drug_name, dosage, is_repeat, date_start, date_end);
+    }
+    
+    public void retreivePrescriptions(
+            DBConnection dbcon,
+            int clientid
+    ) {
+        String query = "SELECT employeeid, drug_name, dosage, is_repeat, "
+                + "date_start, date_end FROM Prescriptions WHERE clientid = "
+                + clientid;
+        
+        //Set up lists to record data
+        List<Integer> employeeList = new ArrayList<Integer>();
+        List<String> drugList = new ArrayList<String>();
+        List<String> doseList = new ArrayList<String>();
+        List<Boolean> repeatList = new ArrayList<Boolean>();
+        List<String> startList = new ArrayList<String>();
+        List<String> endList = new ArrayList<String>();
+        
+        try (Statement stmt = dbcon.conn.createStatement()) {
+            ResultSet resultSet = stmt.executeQuery(query);
+            while (resultSet.next()) {
+                employeeList.add(resultSet.getInt("employeeid"));
+                drugList.add(resultSet.getString("drug_name"));
+                doseList.add(resultSet.getString("dosage"));
+                repeatList.add(resultSet.getBoolean("is_repeat"));
+                startList.add(resultSet.getString("date_start"));
+                endList.add(resultSet.getString("date_end"));
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        
+        // Convert lists to arrays
+        int list_len = employeeList.size();
+        LocalDate[] startArr = new LocalDate[list_len];
+        LocalDate[] endArr = new LocalDate[list_len];
+        int[] employeeArr = new int[list_len];
+        // Convert date formats and integer list
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        for (int i = 0; i < list_len; i++) {
+            startArr[i] = LocalDate.parse(startList.get(0), formatter);
+            endArr[i] = LocalDate.parse(endList.get(0), formatter);
+            employeeArr[i] = employeeList.get(i).intValue();
+        }
+        
+        String drugArr[] = new String[list_len];
+        drugArr = drugList.toArray(drugArr);
+        String dosageArr[] = new String[list_len];
+        dosageArr = doseList.toArray(dosageArr);
+        Boolean repeatArr[] = new Boolean[list_len];
+        repeatArr = repeatList.toArray(repeatArr);
+        
+        this.setClientId(clientid);
+        this.setEmployeeId(employeeArr);
+        this.setDrugName(drugArr);
+        this.setDosage(dosageArr);
+        this.setIsRepeat(repeatArr);
+        this.setDateStart(startArr);
+        this.setDateEnd(endArr);
     }
 }
