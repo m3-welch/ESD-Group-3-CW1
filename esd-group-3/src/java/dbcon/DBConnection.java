2c15fe309875f5a5f128d605f9d57c10bca1c41e
Harrison <harrison.be@hotmail.co.uk>
2021-01-14 17:29:05 +0000

    Import cleanup

--- esd-group-3/src/java/dbcon/DBConnection.java
+++ esd-group-3/src/java/dbcon/DBConnection.java
@@ -7,12 +7,7 @@ package dbcon;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import models.Client;
-import models.User;
-import models.Employee;
 
 /**
  *
