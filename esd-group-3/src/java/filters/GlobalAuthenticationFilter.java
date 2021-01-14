83b23411ffa0a777b02ee12dbe7f1d291d6cbcf5
Austi <Austi@LAPTOP-243CT3AT>
2021-01-14 19:58:49 +0000

    Looks like its working. TODO: - Test extensively, for all possible cases - Work out urls, they match servlets instead of jsp pages etc.

--- esd-group-3/src/java/filters/GlobalAuthenticationFilter.java
+++ esd-group-3/src/java/filters/GlobalAuthenticationFilter.java
@@ -19,7 +19,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-@WebFilter("/AuthenticationFilter")
+@WebFilter("/GlobalAuthenticationFilter")
 public class GlobalAuthenticationFilter implements Filter {
 
 	private ServletContext context;
@@ -38,29 +38,104 @@ public class GlobalAuthenticationFilter implements Filter {
 		this.context.log("Requested Resource::"+uri);
 		
 		HttpSession loginSession = req.getSession(false);
-		
-                // pages that do not require any authentication
-                if(loginSession == null && (uri.endsWith("login.jsp") || uri.endsWith("signup.jsp") || uri.endsWith("LoginServlet.java") || uri.endsWith("SignupServlet"))){
-                    // if no session, continue
-                    chain.doFilter(request, response);
+                
+                // 0 = bad login, 1 = doctor, 2 = nurse, 3 = client, 4 = admin
+                Integer role = 0;
+                if(loginSession != null){
+                    role = (Integer) loginSession.getAttribute("role");
+                    if(role == null){
+                        role = 0;
+                    }
                 }
-                else if(loginSession != null && (uri.endsWith("login.jsp") || uri.endsWith("signup.jsp") || uri.endsWith("LoginServlet.java") || uri.endsWith("SignupServlet"))){
-                    // if user is logged in, redirect to dashboard and display message
-                    res.sendRedirect("home.jsp");
+                else {
+                    role = 0;
                 }
                 
-                //
-		if(loginSession == null && !(uri.endsWith("login.jsp") || uri.endsWith("signup.jsp"))){
-			this.context.log("Unauthorized access request");
-			res.sendRedirect("login.jsp");
-		}else{
+                // set the name for user roles, for dashboard redirects
+                String user_role = "";
+                if(role != 0){
+                    switch(role) {
+                        case 1:
+                            user_role = "doctor";
+                            break;
+                        case 2:
+                            user_role = "nurse";
+                            break;
+                        case 3:
+                            user_role = "client";
+                            break;
+                        case 4:
+                            user_role = "admin";
+                            break;
+                    }
+                }
+		
+                // admin, access all pages
+                if(role == 4){
+                    chain.doFilter(request, response);
+                }
+                // not admin, pages that require admin authenticaion
+                else if(role != 4 && (uri.endsWith("admin.jsp") || uri.endsWith("admin_home.jsp") || uri.endsWith("InvoiceViewerServlet") || uri.endsWith("InvoiceDownloadServlet"))){
+                    // if user is not admin, redirect to dashboard and display message
+                    this.context.log("Unauthorized access request");
+                    req.setAttribute("message", "ERROR - User is not an Administrator"); // Will be available as ${message}
+                    req.getRequestDispatcher("dashboards/" + user_role + "_home.jsp").forward(request,response);
+                    res.sendRedirect("dashboards/" + user_role + "_home.jsp");
+                }
+                // not dr, pages that require dr authenticaion
+                else if(role != 1 && (uri.endsWith("doctor_home.jsp"))){
+                    // if user is not dr, redirect to dashboard and display message
+                    this.context.log("Unauthorized access request");
+                    req.setAttribute("message", "ERROR - User is not a Doctor"); // Will be available as ${message}
+                    req.getRequestDispatcher("dashboards/" + user_role + "_home.jsp").forward(request,response);
+                    res.sendRedirect("dashboards/" + user_role + "_home.jsp");
+                }
+                // not nurse, pages that require nurse authenticaion
+                else if(role != 2 && (uri.endsWith("nurse_home.jsp"))){
+                    // if user is not nurse, redirect to dashboard and display message
+                    this.context.log("Unauthorized access request");
+                    req.setAttribute("message", "ERROR - User is not a Nurse"); // Will be available as ${message}
+                    req.getRequestDispatcher("dashboards/" + user_role + "_home.jsp").forward(request,response);
+                    res.sendRedirect("dashboards/" + user_role + "_home.jsp");
+                }
+                // not patient, pages that require patient authenticaion
+                else if(role != 1 && (uri.endsWith("client_home.jsp"))){
+                    // if user is not dr, redirect to dashboard and display message
+                    this.context.log("Unauthorized access request");
+                    req.setAttribute("message", "ERROR - User is not a Patient"); // Will be available as ${message}
+                    req.getRequestDispatcher("dashboards/" + user_role + "_home.jsp").forward(request,response);
+                    res.sendRedirect("dashboards/" + user_role + "_home.jsp");
+                }
+                // login, pages that do not require any authentication
+                else if(role != 0 && (uri.endsWith("login.jsp") || uri.endsWith("signup.jsp") || uri.endsWith("LoginServlet") || uri.endsWith("SignupServlet") || uri.endsWith("esd-group-3/"))){
+                    // if user is logged in, redirect to dashboard and display message
+                    this.context.log("Unauthorized access request");
+                    req.setAttribute("message", "ERROR - Please Logout to Signup or Login"); // Will be available as ${message}
+                    req.getRequestDispatcher("dashboards/" + user_role + "_home.jsp").forward(request,response);
+                    res.sendRedirect("dashboards/" + user_role + "_home.jsp");
+                }
+                // no login, pages that require any authentication
+                else if(role == 0){
+                    if (uri.endsWith("login.jsp") || uri.endsWith("signup.jsp") || uri.endsWith("LoginServlet") || uri.endsWith("SignupServlet") || uri.endsWith("esd-group-3/")){
+                        chain.doFilter(request, response);
+                    }
+                    else{
+                        // if no user logged in, and page requires authentication, send to login
+                        this.context.log("Unauthorized access request");
+                        req.setAttribute("message", "ERROR - Please Login"); // Will be available as ${message}
+                        req.getRequestDispatcher("login.jsp").forward(request,response);
+                        res.sendRedirect("login.jsp");
+                    }
+                }
+                else{
 			// pass the request along the filter chain
 			chain.doFilter(request, response);
 		}
+                
 	}
 
 	public void destroy() {
-		//close any resources here
+            //close any resources here
 	}
 
 }
