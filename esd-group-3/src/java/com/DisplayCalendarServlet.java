86cee4da36ac7c0bd8adabf3e491edf85c243ee9
Harrison <harrison.be@hotmail.co.uk>
2021-01-18 13:08:09 +0000

    Removed listener, working on calendar work

--- esd-group-3/src/java/com/DisplayCalendarServlet.java
+++ esd-group-3/src/java/com/DisplayCalendarServlet.java
@@ -12,11 +12,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import dbcon.DBConnection;
 import java.sql.SQLException;
-import java.util.ArrayList;
+import java.time.LocalDate;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.http.HttpSession;
-import models.Events;
+import models.Operation;
 import models.User;
 
 /**
@@ -28,18 +28,26 @@ public class DisplayCalendarServlet extends HttpServlet {
             throws ServletException, IOException {
         
         response.setContentType("text/html");
-        HttpSession loginSession = request.getSession();
+        HttpSession loginSession = request.getSession(false);
         request.getRequestDispatcher((String)loginSession.getAttribute("dashboard")).include(request, response);
+        
+        int userid = (Integer)loginSession.getAttribute("userID");
+        
         
         DBConnection dbcon;
         try {
             dbcon = new DBConnection("smartcaretest", "", "");
-            int userid = Integer.parseInt(request.getParameter("userid"));
             User user = new User();
             user.retrieveByUserId(dbcon, userid);
             user.events.getOperationsFromDB(dbcon, user.getId());
             
-            String startDate = request.getParameter("start"); // TODO: CONVERT IT
+            LocalDate startDate = LocalDate.parse(request.getParameter("start"));
+            LocalDate endDate = startDate.plusWeeks(1);
+            
+            Operation[] ops = user.events.getEventsBetweenDates(startDate, endDate);
+            
+            request.setAttribute("message", "Data Loaded Successfully");
+            request.setAttribute("data", ops);
             
         } catch (SQLException ex) {
             Logger.getLogger(DisplayCalendarServlet.class.getName()).log(Level.SEVERE, null, ex);
