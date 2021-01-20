3fe7e9f56f3040c57a92b1b33f959e01a0c4e4b3
Mrbeekon <kirkysam9@gmail.com>
2021-01-20 15:43:17 +0000

    Hold

--- esd-group-3/src/java/com/ViewUsersServlet.java
+++ esd-group-3/src/java/com/ViewUsersServlet.java
@@ -5,82 +5,105 @@
  */
 package com;
 
+import dbcon.DBConnection;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import models.Client;
+import models.Employee;
+import models.User;
 
 /**
  *
  * @author Sam
  */
-public class ViewUsersServlet extends HttpServlet {
-
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ViewUsersServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ViewUsersServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+public class ViewUsersServlet extends HttpServlet{
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession loginSession = request.getSession(false);
+        request.setAttribute("dashboard", "/esd-group-3/dashboards/" + loginSession.getAttribute("user_role") + "_home.jsp");
+        response.setContentType("text/html");
+        
+        request.getRequestDispatcher("pages/ViewUsers.jsp").include(request, response);
+        
+        User user = new User();
+        List<User> users = null;        
+        
+        try {
+            DBConnection dbcon = new DBConnection("smartcaretest", "", "");
+        } catch (SQLException ex) {
+            Logger.getLogger(SignupServlet.class.getName()).log(Level.SEVERE, null, ex);
         }
+        
+        String outputList = "<table class='patients-table'>";
+        
+        for (int i = 0; i < users.size(); i++) {
+            outputList += "<tr><form action='ViewPatientsServlet' method='POST'><td><input type='text' value='" + users.get(i).getClientId() + "' name='clientId' readonly>" + "</td><td>" +
+                    users.get(i).getFirstname() + " " + users.get(i).getLastname() + "</td><td>" +
+                    (users.get(i).getIsNhs().equals("true") ? "NHS" : "Private") + "</td><td>" +
+                    users.get(i).getAddress() + "</td><td>" +
+                    "<input type='submit' name='delete_patient' value='delete' class='button'/></td></form></tr>";
+        }
+        
+        outputList += "</table>";
+        request.setAttribute("patientlist", outputList);
     }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
+    
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
+                            throws ServletException, IOException {
+        response.setContentType("text/html");
+        HttpSession loginSession = request.getSession(false);
+        request.setAttribute("dashboard", "/esd-group-3/dashboards/" + loginSession.getAttribute("user_role") + "_home.jsp");
+        
+        request.getRequestDispatcher("pages/ViewEmployees.jsp").include(request, response);
+        
+        //decare vars
+        String filter = request.getParameter("filter");
+        
+        if ("doctor".equals(filter)) {
+            request.setAttribute("checkeddoctor", "checked='true'");
+            request.setAttribute("checkednurse", "");
+            request.setAttribute("checkedcombined", "");
+        } else if ("nurse".equals(filter)) {
+            request.setAttribute("checkeddoctor", "");
+            request.setAttribute("checkednurse", "checked='true'");
+            request.setAttribute("checkedcombined", "");
+        } else if ("all".equals(filter)) {
+            request.setAttribute("checkeddoctor", "");
+            request.setAttribute("checkednurse", "");
+            request.setAttribute("checkedcombined", "checked='true'");
+        }
+        
+        Employee emp = new Employee();
+        List<Employee> employees = null;
+        
+        try {
+            DBConnection dbcon = new DBConnection("smartcaretest", "", "");
+            employees = emp.filteredRetrieveAllEmployees(dbcon, filter);
+        } catch (SQLException ex) {
+            Logger.getLogger(SignupServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        String outputList = "<table class='patients-table'>";
+        
+        for (int i = 0; i < employees.size(); i++) {
+            outputList += "<tr><td>" + employees.get(i).getEmployeeId() + 
+                    "</td><td>" + employees.get(i).getFirstname() + " " + 
+                    employees.get(i).getLastname() + "</td><td>" + 
+                    employees.get(i).getRole() + "</td><td>" + 
+                    employees.get(i).getEmail() + "</td><td>" + 
+                    employees.get(i).getAddress() + "</td></tr>";
+        }
+        
+        outputList += "</table>";
+        request.setAttribute("employeelist", outputList);
+        request.getRequestDispatcher("pages/ViewUsers.jsp").forward(request,response);
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
 }
