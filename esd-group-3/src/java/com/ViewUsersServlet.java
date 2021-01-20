b2de577263d10753b032d2f52c8fb21ebeda82cd
Mrbeekon <kirkysam9@gmail.com>
2021-01-20 16:07:00 +0000

    Up to date front end

--- esd-group-3/src/java/com/ViewUsersServlet.java
+++ esd-group-3/src/java/com/ViewUsersServlet.java
@@ -7,7 +7,6 @@ package com;
 
 import dbcon.DBConnection;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.logging.Level;
@@ -17,7 +16,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import models.Client;
 import models.Employee;
 import models.User;
 import models.Client;
@@ -27,136 +25,112 @@ import models.Client;
  * @author Sam
  */
 public class ViewUsersServlet extends HttpServlet{
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+                            throws ServletException, IOException {
+        response.setContentType("text/html");
         HttpSession loginSession = request.getSession(false);
         request.setAttribute("dashboard", "/esd-group-3/dashboards/" + loginSession.getAttribute("user_role") + "_home.jsp");
-        response.setContentType("text/html");
         
         request.getRequestDispatcher("pages/ViewUsers.jsp").include(request, response);
         
-        User user = new User();
-        List<User> users = null;        
+        //decare vars
+        String save = "";
+        String uname = "";
+        String role = "";
+        String dbRole = "";
         
-        try {
-            DBConnection dbcon = new DBConnection("smartcaretest", "", "");
-        } catch (SQLException ex) {
-            Logger.getLogger(SignupServlet.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
-        String outputList = "<table class='patients-table'>";
-        
-        for (int i = 0; i < users.size(); i++) {
-            outputList += "<tr><form action='ViewPatientsServlet' method='POST'><td><input type='text' value='" + clients.get(i).getClientId() + "' name='clientId' readonly>" + "</td><td>" +
-                    clients.get(i).getFirstname() + " " + users.get(i).getLastname() + "</td><td>" +
-                    (userss.get(i).getIsNhs().equals("true") ? "NHS" : "Private") + "</td><td>" +
-                    clients.get(i).getAddress() + "</td><td>" +
-                    "<input type='submit' name='delete_patient' value='delete' class='button'/></td></form></tr>";
-        }
-        
-        outputList += "</table>";
-        request.setAttribute("patientlist", outputList);
-    }
-    
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-                            throws ServletException, IOException {
-        
-        /*
-        String delete = "";
         
         //request.getRequestDispatcher("admin_home.jsp").include(request, response);
         
         try{
-            delete = request.getParameter("delete_patient");            
+            save = request.getParameter("save");
+            uname = request.getParameter("username");
+            role = request.getParameter("role");     
         }
         catch(Exception e){
             System.out.println(e);
         }
         
-        if ("delete".equals(delete)) {
+        if ("save".equals(save)) {
             try {
                 DBConnection dbcon = new DBConnection("smartcaretest", "", "");
-                Client deleteClient = new Client();
-                deleteClient.retrieveClientByIdDrop(dbcon, Integer.parseInt(request.getParameter("clientId")));
-                deleteClient.dropUserById(dbcon, deleteClient.getClientId());
+                User updateUser = new User();// create a client object to update the old one              
+                User dbUser = new User();
+                
+                dbUser.retrieveByUsername(dbcon, uname);
+                updateUser.retrieveByUsername(dbcon, uname);
+                updateUser.setRole(role);
+                dbRole = dbUser.getRole();
+                if (("doctor".equals(role))||("nurse".equals(role))||("admin".equals(role))) {
+                    if("doctor".equals(dbRole)||("nurse".equals(dbRole))||("admin".equals(dbRole))){//if update user and table user are of the same client or employee then just change role
+                        dbUser.editUser(dbcon, "Users", dbUser.getUsername(), "Role", updateUser.getRole());//just change type in users
+                    } else {
+                        //remove dbUser from employee db
+                        dbUser.dropUser(dbcon, dbUser.getUsername());
+                        //add updateUser to client
+                        Employee updateEmployee = new Employee();
+                        updateEmployee.create(dbcon, updateUser.getUsername(), updateUser.getPassword(), updateUser.getFirstname(), updateUser.getLastname(), updateUser.getEmail(), updateUser.getAddress(), updateUser.getRole(), "True");
+                    }
+                        
+                } else if("client".equals(role)){
+                   if(!("client".equals(dbRole))){
+                        //remove dbUser from client db
+                        dbUser.dropUser(dbcon, dbUser.getUsername());
+                        //add updateUser to employee db
+                        Employee updateEmployee = new Employee();
+                        updateEmployee.create(dbcon, updateUser.getUsername(), updateUser.getPassword(), updateUser.getFirstname(), updateUser.getLastname(), updateUser.getEmail(), updateUser.getAddress(), updateUser.getRole(), "True");
+                        Client updateClient = new Client();
+                        updateClient.create(dbcon, updateUser.getUsername(), updateUser.getPassword(), updateUser.getFirstname(), updateUser.getLastname(), updateUser.getEmail(), updateUser.getAddress(), updateUser.getRole(), "True");
+                    } 
+                }
+                
             } catch (SQLException ex) {
                 Logger.getLogger(ViewPatientsServlet.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
         
-        //decare vars
-        String filter = request.getParameter("filter");
-        
-        if ("NHS".equals(filter)) {
-            request.setAttribute("checkednhs", "checked='true'");
-        } else if ("private".equals(filter)) {
-            request.setAttribute("checkedprivate", "checked='true'");
-        } else if ("all".equals(filter)) {
-            request.setAttribute("checkedcombined", "checked='true'");
-        }
-        
-        Client client = new Client();
-        List<Client> clients = null;        
+        User user = new Client();
+        List<User> users = null;        
         
         try {
             DBConnection dbcon = new DBConnection("smartcaretest", "", "");
-            clients = client.getAll(dbcon, filter);
+            users = user.retrieveAll(dbcon);
         } catch (SQLException ex) {
-            Logger.getLogger(SignupServlet.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println(ex);
         }
         
-        String outputList = "<table class='patients-table'>";
+        String outputList = "<table class='users-table'>";
         
-        for (int i = 0; i < clients.size(); i++) {
-            outputList += "<tr><form action='ViewPatientsServlet' method='POST'><td><input type='text' value='" + clients.get(i).getClientId() + "' name='clientId' readonly>" + "</td><td>" +
-                    clients.get(i).getFirstname() + " " + clients.get(i).getLastname() + "</td><td>" +
-                    (clients.get(i).getIsNhs().equals("true") ? "NHS" : "Private") + "</td><td>" +
-                    clients.get(i).getAddress() + "</td><td>" +
-                    "<input type='submit' name='delete_patient' value='delete' class='button'/></td></form></tr>";
-        }
-        
-        */
-        response.setContentType("text/html");
-        HttpSession loginSession = request.getSession(false);
-        request.setAttribute("dashboard", "/esd-group-3/dashboards/" + loginSession.getAttribute("user_role") + "_home.jsp");
-        
-        request.getRequestDispatcher("pages/ViewUsers.jsp").include(request, response);
-        
-        //decare vars
-        String filter = request.getParameter("filter");
-        
-        if ("doctor".equals(filter)) {
-            request.setAttribute("checkeddoctor", "checked='true'");
-            request.setAttribute("checkednurse", "");
-            request.setAttribute("checkedcombined", "");
-        } else if ("nurse".equals(filter)) {
-            request.setAttribute("checkeddoctor", "");
-            request.setAttribute("checkednurse", "checked='true'");
-            request.setAttribute("checkedcombined", "");
-        } else if ("all".equals(filter)) {
-            request.setAttribute("checkeddoctor", "");
-            request.setAttribute("checkednurse", "");
-            request.setAttribute("checkedcombined", "checked='true'");
-        }
-        
-        Employee emp = new Employee();
-        List<Employee> employees = null;
-        
-        try {
-            DBConnection dbcon = new DBConnection("smartcaretest", "", "");
-            employees = emp.filteredRetrieveAllEmployees(dbcon, filter);
-        } catch (SQLException ex) {
-            Logger.getLogger(SignupServlet.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
-        String outputList = "<table class='patients-table'>";
-        
-        for (int i = 0; i < employees.size(); i++) {
-            outputList += "<tr><td>" + employees.get(i).getEmployeeId() + 
-                    "</td><td>" + employees.get(i).getFirstname() + " " + 
-                    employees.get(i).getLastname() + "</td><td>" + 
-                    employees.get(i).getRole() + "</td><td>" + 
-                    employees.get(i).getEmail() + "</td><td>" + 
-                    employees.get(i).getAddress() + "</td></tr>";
+        for (int i = 0; i < users.size(); i++) {
+            String selectStatement = "";
+            if("doctor".equals(users.get(i).getRole())){
+                selectStatement = "<option selected='selected' value='doctor'>doctor</doctor>" +
+                        "<option value='nurse'>nurse</option>" +
+                        "<option value='admin'>admin</option>" +
+                        "<option value='client'>client</option>";
+            } else if("nurse".equals(users.get(i).getRole())) {
+                selectStatement = "<option value='doctor'>doctor</doctor>" +
+                        "<option selected='selected' value='nurse'>nurse</option>" +
+                        "<option value='admin'>admin</option>" +
+                        "<option value='client'>client</option>";
+            }else if("admin".equals(users.get(i).getRole())) {
+                selectStatement = "<option value='doctor'>doctor</doctor>" +
+                        "<option value='nurse'>nurse</option>" +
+                        "<option selected='selected' value='admin'>admin</option>" +
+                        "<option value='client'>client</option>";
+            }else if("client".equals(users.get(i).getRole())) {
+                selectStatement = "<option value='doctor'>doctor</doctor>" +
+                        "<option value='nurse'>nurse</option>" +
+                        "<option value='admin'>admin</option>" +
+                        "<option selected='selected' value='client'>client</option>";
+            }
+            outputList += "<tr><form action='EditUserServlet' method='POST'><td>" + users.get(i).getId() + "</td><td>" +
+                    "<input type='text' name='username' value='" + users.get(i).getUsername() + "' readonly/></td><td>" +
+                    users.get(i).getFirstname() + " " + users.get(i).getLastname() + "</td><td>" +
+                    users.get(i).getEmail() + "</td><td>" +
+                    users.get(i).getAddress() + "</td><td>" +
+                    "<select name='role'>" + selectStatement + "</select></td><td>" +
+                    "<input type='submit' name='save' value='save' class='button'/></td></form></tr>";
         }
         
         outputList += "</table>";
